3c6c463cec40103896651eb1b00e561ee28b418e
NikitaDevochko <devochko05071999@gmail.com>
2021-10-14 22:23:31 +0300

    added 1st lvl

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : Pool<Bullet>
+{
+    private void Awake()
+    {
+        Init();
+    }
+}
